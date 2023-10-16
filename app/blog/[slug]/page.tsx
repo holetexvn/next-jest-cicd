8f6d84ad2efcb205f68dd86aca347d53d4cbe597
HoleTex <80536553+holetexvn@users.noreply.github.com>
2023-10-16 21:46:20 +0700

    Revert "release"

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,10 +4,13 @@ type Params = {
   }
 }
 
+export async function generateMetadata({ params }: Params) {
+  return { title: `Post: ${params.slug}` }
+}
+
 export default function Page({ params }: Params) {
   return <>
   <h1>Slug: {params.slug}</h1>
   <p>HoleTex</p>
-  <p>Welcome to HCM</p>
   </>
 }
