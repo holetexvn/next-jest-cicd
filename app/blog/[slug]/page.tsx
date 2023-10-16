42ad8ba9bdff99725f65d761af332eca9f8c5738
holetex <holetex@outlook.com>
2023-10-16 21:12:36 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,7 +11,7 @@ export async function generateMetadata({ params }: Params) {
 export default function Page({ params }: Params) {
   return <>
   <h1>Slug: {params.slug}</h1>
-  <p>Hole Tex</p>
+  <p>HoleTex</p>
   <p>Welcome to HCM</p>
   </>
 }
