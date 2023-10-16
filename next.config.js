965ccf5cb872a055891ce6d89b81c9eee7be1404
holetex <holetex@outlook.com>
2023-10-16 21:29:25 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@
  */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true
-  }
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
