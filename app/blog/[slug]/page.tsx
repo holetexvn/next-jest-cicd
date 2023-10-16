d324516efc33e6f91a3c5eda52468d1fc2fb9f22
holetex <holetex@outlook.com>
2023-10-16 21:36:14 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,10 +4,6 @@ type Params = {
   }
 }
 
-export async function generateMetadata({ params }: Params) {
-  return { title: `Post: ${params.slug}` }
-}
-
 export default function Page({ params }: Params) {
   return <>
   <h1>Slug: {params.slug}</h1>
--- app/rsc/page.tsx
+++ app/rsc/page.tsx
@@ -1,9 +1,5 @@
 import 'server-only'
 
-export const metadata = {
-  title: 'App Router',
-}
-
 export default function Page() {
   return <h1>App Router</h1>
 }
