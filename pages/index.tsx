f53bea009cb91594cfaf0cb4ab3cb837c5b5df07
holetex <holetex@outlook.com>
2023-10-16 22:29:54 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">HoleTex</a>
         </h1>
 
         <p className={styles.description}>
